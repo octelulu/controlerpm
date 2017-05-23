f87aee3306c20fb845d747d86d4d8fb443441be0
Luís Filipe Soutello <luisfmgs@gmail.com>
2017-05-22 23:21:41 -0300

    Initial commit

---
+++ Controle.ino
@@ -0,0 +1,31 @@
+#include <TimerOne.h>
+
+const int PWM_Pin = 3;
+const int IR_Pin = 4;  
+
+/* Rotina da interrupção de tempo */
+void timeInterrupt() {
+  
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PWM_Pin, OUTPUT);
+  pinMode(IR_Pin, INPUT);
+  
+  // Configuração da interrupção de tempo
+  Timer1.initialize(2000); 
+  Timer1.attachInterrupt( timeInterrupt );
+}
+
+}
+
+void loop() {
+  analogWrite(PWM_Pin, 127);
+  Serial.print(digitalRead(IR_Pin));
+  Serial.print("\n");  
+  delay(100);
+  
+}
+
